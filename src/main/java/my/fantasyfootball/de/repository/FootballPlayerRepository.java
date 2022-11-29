782838f7fc445cd42393b7ac8b592828af34d6a8
skydan11 <oskidan8684@gmail.com>
2022-11-29 19:40:21 +0100

    deleted src

--- src/main/java/my/fantasyfootball/de/repository/FootballPlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package my.fantasyfootball.de.repository;
-
-import my.fantasyfootball.de.model.footballplayer.FootballPlayer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface FootballPlayerRepository extends JpaRepository<FootballPlayer, UUID> {
-}
--- src/main/java/my/fantasyfootball/de/repository/StatisticsRepository.java
+++
@@ -1,9 +0,0 @@
-package my.fantasyfootball.de.repository;
-
-import my.fantasyfootball.de.model.Statistics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface StatisticsRepository extends JpaRepository<Statistics, UUID> {
-}
